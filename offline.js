4b507d0ce35920cb183ffc44614280821f31008c
reaksi-eksoterm-endoterm <74961040+reaksi-eksoterm-endoterm@users.noreply.github.com>
2020-11-24 17:44:49 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,33 @@
+﻿{
+	"version": 1606214442,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/p-sheet0.png",
+		"images/p-sheet1.png",
+		"images/tiledbackground3.png",
+		"images/notice-sheet0.png",
+		"images/cao2-sheet0.png",
+		"images/detergen2-sheet0.png",
+		"images/urea2-sheet0.png",
+		"images/asamsitrat-sheet0.png",
+		"images/reset2-sheet0.png",
+		"images/s-sheet0.png",
+		"images/partikel-sheet0.png",
+		"images/lovepik_com401062940handdrawndirectionalarrow-sheet0.png",
+		"images/dasar.png",
+		"images/bg2-sheet0.png",
+		"images/bg-sheet0.png",
+		"images/close-sheet0.png",
+		"images/suhu2.png",
+		"media/bensound-smile (online-audio-converter.com).ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
